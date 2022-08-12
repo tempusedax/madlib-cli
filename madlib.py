b59d341a9e4b42ca81682f2be6822429d937e5bb
Marvin Bonds <marvin.bonds@gmail.com>
2022-08-11 21:28:55 -0500

    working on last bit for input

--- madlib.py
+++ madlib.py
@@ -1,18 +1,27 @@
-
+import re
 
 print("Welcome to Madlib")
 
 
 def read_template (path):
     file = open(path, "r")
+    # except FileNotFoundError:
+    # raise FileNotFoundError("Cannot find {}.".format(PATH_OF_FILE))
     read = file.read()
     file.close()
     return read.strip()
 
 
-def parse_template(words):
-    return True
+def parse_template(template: str):
+
+    parse = re.findall(r'\{(.*?)\}', template)
+
+    for word in parse:
+
+        template = template.replace((word), '')
+
+    return template, tuple(parse)
 
 
-def merge(str, tup):
-    return False
+def merge(string, tup):
+    return string.format(*tup)
